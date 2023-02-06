bd4c1c9ef9df27435138248c2309bc0361747eee
thomas3577 <thomas3577@gmx.net>
2023-02-06 21:42:40 +0100

    Cleaned up code

--- src/task.ts
+++ src/task.ts
@@ -1,5 +1,4 @@
 import { Command, CommandOrExecutorOrOptions, Executor, ICommandOptions, ITask, ITaskMethods, Options, RequiredOrCommandOrExecutor, TaskDefinition } from './definitions.ts';
-import { getCommand, getExecutor, getOptions, getRequired } from './utils.ts';
 
 export class Task implements ITask, ITaskMethods {
   private readonly _name: string;
@@ -74,10 +73,32 @@ export class Task implements ITask, ITaskMethods {
 }
 
 export const task: TaskDefinition = (name: string | ITask, param1?: RequiredOrCommandOrExecutor, param2?: CommandOrExecutorOrOptions, param3?: Options): ITask => {
-  const required: Array<string> = getRequired(param1);
-  const command: Command = getCommand(param1, param2);
-  const executor: Executor = getExecutor(param1, param2);
-  const options: Options = getOptions(param2, param3);
+  let required: Array<string> = [];
+  if (Array.isArray(param1)) {
+    required = param1.map((item) => typeof item === 'object' ? item.name : item).filter((item) => item !== undefined);
+  }
+
+  let command: Command = undefined as unknown as Command;
+  if (typeof param2 === 'string') {
+    command = param2;
+  } else if (typeof param1 === 'string') {
+    command = param1;
+  }
+
+  let executor: Executor = undefined as unknown as Executor;
+  if (typeof param1 === 'function') {
+    executor = param1;
+  } else if (typeof param2 === 'function') {
+    executor = param2;
+  }
+
+  let options: Options = undefined as unknown as Options;
+  if (typeof param2 === 'object') {
+    options = param2;
+  } else if (typeof param3 === 'object') {
+    options = param3;
+  }
+
   const instance: ITask = new Task(name, required, command, executor, options);
 
   return instance;

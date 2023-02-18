8d05c96a355d2f8c7da894fea5f2f17d04ddd5d9
thomas3577 <thomas3577@gmx.net>
2023-02-18 17:16:08 +0100

    Added tests

---
+++ src/tano.factory.test.ts
@@ -0,0 +1,59 @@
+import { assertEquals, assertInstanceOf } from 'std/testing/asserts.ts';
+import { describe, it } from 'std/testing/bdd.ts';
+
+import { getImportUrl } from './tano.factory.ts';
+
+describe(getImportUrl.name, () => {
+  it(`if param is undefined`, async () => {
+    const fileOrUrl = undefined;
+    let actual = 'init';
+
+    try {
+      actual = await getImportUrl(fileOrUrl as unknown as string);
+    } catch (error) {
+      assertInstanceOf(error, TypeError);
+    }
+
+    assertEquals(actual, 'init');
+  });
+
+  it(`if param is null`, async () => {
+    const fileOrUrl = null;
+    let actual = 'init';
+
+    try {
+      actual = await getImportUrl(fileOrUrl as unknown as string);
+    } catch (error) {
+      assertInstanceOf(error, TypeError);
+    }
+
+    assertEquals(actual, 'init');
+  });
+
+  it(`if param is a invalid path`, async () => {
+    const fileOrUrl = 'something';
+    let actual = 'init';
+
+    try {
+      actual = await getImportUrl(fileOrUrl as unknown as string);
+    } catch (error) {
+      assertInstanceOf(error, Error);
+    }
+
+    assertEquals(actual, 'init');
+  });
+
+  it(`if param is a valid url`, async () => {
+    const fileOrUrl = 'https://some.thing/tanofile.ts';
+    const actual = await getImportUrl(fileOrUrl as unknown as string);
+
+    assertEquals(actual, 'https://some.thing/tanofile.ts');
+  });
+
+  it(`if param is a valid url`, async () => {
+    const fileOrUrl = './examples/tanofile.ts';
+    const actual = await getImportUrl(fileOrUrl as unknown as string);
+
+    assertEquals(actual.endsWith('tanofile.ts'), true);
+  });
+});
